db43c0bd99eee5f4e96fb580682cd05f20f28cca
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2023-03-15 15:53:04 -0300

    Create Uri 1071.php

---
+++ Uri 1071.php
@@ -0,0 +1,27 @@
+<?php
+
+// Lê os valores de entrada
+$x = intval(fgets(STDIN));
+$y = intval(fgets(STDIN));
+
+// Caso X seja maior que Y, troca os valores
+if ($x > $y) {
+    $temp = $x;
+    $x = $y;
+    $y = $temp;
+}
+
+// Inicializa a variável que armazenará a soma dos números impares
+$sum = 0;
+
+// Faz um loop de X até Y-1 para verificar quais números são impares e somá-los
+for ($i = $x+1; $i < $y; $i++) {
+    if ($i % 2 != 0) {
+        $sum += $i;
+    }
+}
+
+// Imprime o resultado
+echo $sum . "\n";
+
+?>
